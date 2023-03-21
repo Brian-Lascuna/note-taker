945943170f2cb8ca21dcbe2a9bf0516814ae7ce5
Brian-Lascuna <blascuna@gmail.com>
2023-03-20 17:53:18 -0700

    basic server setup

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+  });
+
+app.listen(PORT, () => {
+  console.log(`Note taker app listening at http://localhost:${PORT}`);
+});
