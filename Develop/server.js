5b7a15c1d2a0fccbd234a2def6c5d9c3b728fdfd
Brian-Lascuna <blascuna@gmail.com>
2023-03-22 16:23:20 -0700

    Added ability to save additional notes to database

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const noteData = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
 const PORT = 3001;
 
@@ -18,6 +21,42 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
   });
+
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+      const newNote = {
+        title,
+        text,
+        note_id : uuidv4()
+      };
+
+      const db = fs.readFileSync('./db/db.json');
+      if (db.length == 0) {
+        fs.writeFile('./db/db.json', JSON.stringify(newNote));
+      } else {
+        const noteObj = JSON.parse(db);
+        noteObj.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(noteObj, null, "\t"), (err) => {
+          if (err) throw err;
+          console.log('Note added successfully');
+        })
+      }
+
+      const response = {
+        status: 'success',
+        body: newNote
+        };
+
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting note')
+    }
+})
 
 app.listen(PORT, () => {
   console.log(`Note taker app listening at http://localhost:${PORT}`);
